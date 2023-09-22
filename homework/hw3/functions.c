055d22818c20ce3642ee33b439b2d14899b21f32
Aprina Wang <awang100@jhu.edu>
2023-09-22 09:38:37 -0400

    add starter code for hw3

---
+++ homework/hw3/functions.c
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+#include <stdio.h>
+#include <string.h>
+
+int read_file(FILE *fp, char words[][MAX_WORD_SIZE + 1], int size) {
+  // TODO: Implement Me!
+}
+
+int match(const char *regex, const char *word, int restriction) {
+  // TODO: Implement me!
+}
+
---
+++ homework/hw3/hw3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "functions.h"
+
+
+#define MAX_WORD_NUM 1024
+#define MAX_INPUT_SIZE 128
+#define DEFAULT_RESTRICTION 10
+
+int main(int argc, const char *argv[]) {
+  char words[ MAX_WORD_NUM ][ MAX_WORD_SIZE + 1 ]; 
+  // TODO: Implement me!
+
+  return 0;
+}
